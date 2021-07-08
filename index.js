2183faa7610fd854962d4d130fcd9df69aa9c0d3
escttsalo <escttsalo@gmail.com>
2021-07-08 11:42:19 -0600

    chain endpoing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 5000
-server.use('/api', (req, res) => {
+server.use('/', (req, res) => {
     res.json({message: "working and ready to serve!"})
 })
 
