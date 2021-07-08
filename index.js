1a9024c04d846428ccda74a277c419b001f084fd
escttsalo <escttsalo@gmail.com>
2021-07-08 12:08:08 -0600

    create react

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 require('dotenv').config()
+const cors = require('cors')
+const path = require('path')
 const express = require('express')
 const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client')))
 const PORT = process.env.PORT || 5000
+
+server.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'))
+})
+
 server.use('/api', (req, res) => {
     res.json({message: "working and ready to serve!"})
+})
+
+server.use((req,res)=> {
+    res.json({message: "sorry could not server"})
 })
 
 server.listen(PORT, () => {
